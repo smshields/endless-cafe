3f26c2cd750696f3011c4bb609c1e8ce6458403d
Jessica Wei <jessicawei9@gmail.com>
2022-05-11 03:59:22 -0700

    + Initializing a chair manager for interactions with external objects

---
+++ GAME-242-Project-2-Endless-Cafe/Assets/Scripts/ChairManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChairManager : MonoBehaviour
+{
+    private List<GameObject> chairList = new List<GameObject>();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
