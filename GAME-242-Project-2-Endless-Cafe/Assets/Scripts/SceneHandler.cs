43c80c2e044e897ca758721139ef71061a6803a3
Jessica Wei <jessicawei9@gmail.com>
2022-05-12 16:46:30 -0700

    + Options UI prefab + Time UI

--- GAME-242-Project-2-Endless-Cafe/Assets/Scripts/SceneHandler.cs
+++ GAME-242-Project-2-Endless-Cafe/Assets/Scripts/SceneHandler.cs
@@ -6,13 +6,21 @@ using UnityEngine.UI;
 
 public class SceneHandler : MonoBehaviour
 {
-    [SerializeField] private bool useSpecificSeed = false;          // If we have a specific seed to test...
-    [SerializeField] private int seed = 0;                              // Define this seed here
+    private bool useSpecificSeed = false;          // If we have a specific seed to test...
+    private int seed = 0;                              // Define this seed here
 
     [SerializeField] private static float timeScale = 1.0f;
     [SerializeField] private Text currentSeedText;
     [SerializeField] private InputField enteredText;
     [SerializeField] private Animator optionMenuAnimation;
+    [SerializeField] private Text timeText;
+    [SerializeField] private int openingHour = 6;
+    [SerializeField] private int openingMinute = 30;
+    [SerializeField] private int closingHour = 0;
+    [SerializeField] private int closingMinute = 0;
+    private static int timeHour = 0;
+    private static int timeMinute = 0;
+    private float nextUpdateTime;
     private static int enteredSeedValue;
     private static bool valueRequested;
     private void Awake()
@@ -28,12 +36,25 @@ public class SceneHandler : MonoBehaviour
 
     void Start()
     {
-        
+        timeHour = openingHour;
+        timeMinute = openingMinute;
+        nextUpdateTime = Time.time + (1.0f / timeScale);
+        timeText.text = TimeToString();
     }
 
     void Update()
     {
-        
+        if (Time.time >= nextUpdateTime) {
+            timeMinute += 1;
+            if (timeMinute >= 60) {
+                timeMinute = 0;
+                timeHour += 1;
+                if (timeHour >= 24)
+                    timeHour = 0;
+            }
+            nextUpdateTime = Time.time + (1.0f / timeScale);
+            timeText.text = TimeToString();
+        }
     }
 
     public static float GetTimeScale() {
@@ -59,7 +80,7 @@ public class SceneHandler : MonoBehaviour
     }
 
     public static void ReloadScene() {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(0);
         if (valueRequested) {
             Random.InitState(enteredSeedValue);
         }
@@ -81,5 +102,23 @@ public class SceneHandler : MonoBehaviour
 
     public void ToggleOptionsMenu() {
         optionMenuAnimation.SetBool("open", !optionMenuAnimation.GetBool("open"));
+    }
+
+    public string TimeToString() {
+        string hstr = timeHour.ToString();
+        string mstr = timeMinute.ToString();
+        if (timeHour < 10)
+            hstr = "0" + hstr;
+        if (timeMinute < 10)
+            mstr = "0" + mstr;
+        return hstr + ":" + mstr;
+    }
+
+    public static int GetCurrentHour() {
+        return timeHour;
+    }
+
+    public static int GetCurrentMinute() {
+        return timeMinute;
     }
 }
