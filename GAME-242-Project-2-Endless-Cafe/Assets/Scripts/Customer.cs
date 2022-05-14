3b3238c1f0797d09151710bc30b6aec004069c05
Samuel Shields <samuel.m.shields@gmail.com>
2022-05-14 10:37:17 -0700

    bathroom code, start of order/tables

--- GAME-242-Project-2-Endless-Cafe/Assets/Scripts/Customer.cs
+++ GAME-242-Project-2-Endless-Cafe/Assets/Scripts/Customer.cs
@@ -18,15 +18,25 @@ public class Customer : MonoBehaviour
     public float initialLeaveChance = 0.01f; //01%
 
     //Variables indicating likelihood of next action after recieving a drink
-    public float leaveAfterDrinkChance = 1f;
-    public float sitAfterDrinkChance = 0.55f;
-    public float restroomAfterDrinkChance = 0.1f;
+    public float leaveAfterDrinkChance = 1f; //45%
+    public float sitAfterDrinkChance = 0.55f; //45%
+    public float restroomAfterDrinkChance = 0.1f; //10%
+
+    //Variables indicating likelihood of next action after using the bathroom
+    public float leaveAfterBathroomChance = 1.0f;
+    public float sitAfterBathroomChance = 0.67f;
+    public float drinkAfterBathroomChance = 0.33f;
 
     //probability that an order is online or not
     public float onlineChance = 0.05f; //50%
 
     //Tracks if the customer's drink order was online or not.
     public bool onlineOrder = false;
+
+    //Tracks if a customer has already done an action within the Cafe
+    public bool hasDrink = false;
+    public bool hasUsedBathroom = false;
+    public bool hasSatAtTable = false;
 
     //references to customer's goal at any given time
     public enum customerGoal { 
@@ -59,21 +69,31 @@ public class Customer : MonoBehaviour
         {
             this.goal = customerGoal.leaveCafe;
         }
-        else if (initialGoal <= this.initialRestroomChance)
+        else if (initialGoal <= this.initialRestroomChance && !this.hasUsedBathroom)
         {
             this.goal = customerGoal.useRestroom;
+            this.store.BathroomLineEnqueue(this);
         }
         else if (initialGoal <= this.initialSitGoalChance)
         {
             this.goal = customerGoal.sitDown;
         }
-        else 
+        else if(initialGoal <= this.initialDrinkGoalChance && !this.hasDrink)
         {
             this.goal = customerGoal.getDrink;
             //Deterrmine if order is online or not
             this.onlineOrder = (Random.Range(0f, 1f) <= this.onlineChance);
-            //if it's online, join pickup area set.
-            this.store.OrderWaitEnqueue(this);
+            if (this.onlineOrder)
+            {
+                //if it's online, join pickup area queue.
+                this.store.OrderWaitEnqueue(this);
+            }
+            else 
+            {
+                //If it's in person, join order area queue.
+                this.store.OrderLineEnqueue(this);
+            }
+
         }
 
         //
@@ -87,10 +107,12 @@ public class Customer : MonoBehaviour
 
     public void ReceiveDrink() 
     {
+        this.hasDrink = true;
         //Customer can now: sit, leave, use restroom
         float goalAfterDrink = Random.Range(0f, 1f);
-        if (goalAfterDrink <= restroomAfterDrinkChance) {
+        if (goalAfterDrink <= restroomAfterDrinkChance && !this.hasUsedBathroom) {
             this.goal = customerGoal.useRestroom;
+            this.store.BathroomLineEnqueue(this);
         }
         else if (goalAfterDrink <= sitAfterDrinkChance) {
             this.goal = customerGoal.sitDown;
@@ -100,6 +122,31 @@ public class Customer : MonoBehaviour
             this.goal = customerGoal.leaveCafe;
         }
     }
+
+    public void UseBathroom() 
+    {
+        this.hasUsedBathroom = true;
+        //Customer can now: sit, get drink, leave
+        float goalAfterBathroom = Random.Range(0f, 1f);
+        if (goalAfterBathroom <= drinkAfterBathroomChance && !this.hasDrink)
+        {
+            this.goal = customerGoal.getDrink;
+            //TODO: 
+        }
+        else if (goalAfterBathroom <= sitAfterDrinkChance && !this.hasSatAtTable)
+        {
+            this.goal = customerGoal.sitDown;
+        }
+        else
+        {
+            //Destroy here?
+            this.goal = customerGoal.leaveCafe;
+        }
+
+    }
+
+    //Destroy customer, update store statistics
+    public void LeaveCafe() { }
     //Set State
 
     //State updates
--- GAME-242-Project-2-Endless-Cafe/Assets/Scripts/StoreManager.cs
+++ GAME-242-Project-2-Endless-Cafe/Assets/Scripts/StoreManager.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class StoreManager : MonoBehaviour
 {
     //Order Line
-    public Queue<Customer> orderLine;
+    public Queue<Customer> orderLine = new Queue<Customer>(); //Queue of customer objects waiting to place an order
+    public GameObject orderLineGO; //game object for location movement
+    public bool cashierAvailable = true; //indicates if the line is open for a new customer
+    public float orderTimeMinInterval = 2f; //shortest time a customer can take to place an order
+    public float orderTimeMaxInterval = 7f; //longest time a customer can take to place an order
+    public Customer customerPlacingOrder; //current customer placing order
 
     //Order Wait
     public Queue<Customer> orderWait = new Queue<Customer>(); //Queue of customer objects waiting for order
@@ -16,7 +21,11 @@ public class StoreManager : MonoBehaviour
     public float nextOrderWait = 0f; //time until next order is given to a customer 
 
     //Bathroom Line
-    public Queue<Customer> bathroomLine;
+    public Queue<Customer> bathroomLine = new Queue<Customer>(); //Queue of customer objects waiting for order 
+    public bool bathroomVacant = true; //indicates if the bathroom is currently occupied or not.
+    public Customer customerInBathroom; //current customer in bathroom
+    public float bathroomWaitMinInterval = 3f; //shortest time a customer can USE the bathroom
+    public float bathroomWaitMaxInterval = 15f; //longest time a cusomer can USE the bathroom
 
     //Seating
     public Dictionary<int, Customer> seatingWait;
@@ -39,8 +48,14 @@ public class StoreManager : MonoBehaviour
             this.orderWaitDuration = Random.Range(this.orderWaitMinInterval, this.orderWaitMaxInterval);
             this.nextOrderWait = Time.time + this.orderWaitDuration;
         }
+
+        //if the bathroom is vacant and someone is in line, dequeue them and put them into the bathroom
+        if (this.bathroomVacant && this.bathroomLine.Count > 0) {
+            this.BathroomLineDequeue();
+        }
     }
 
+    //Add customer to order waiting
     public int OrderWaitEnqueue(Customer customer) 
     {
         //move customer to order wait
@@ -50,7 +65,7 @@ public class StoreManager : MonoBehaviour
         Debug.Log("Customer started waiting for order. Total customers waiting for order: " + orderWait.Count);
         return orderWait.Count;
     }
-
+    //Remove a customerr from order waiting line
     public int OrderWaitDequeue() 
     {
         //Do not dequeue if no one is waiting.
@@ -59,6 +74,7 @@ public class StoreManager : MonoBehaviour
             //Call customer "order recieved" method
             Customer customer = orderWait.Dequeue();
             Debug.Log("Customer recieved order. Total customers waiting for order: " + orderWait.Count);
+
         }
         return orderWait.Count;
     }
@@ -66,13 +82,31 @@ public class StoreManager : MonoBehaviour
     public int OrderLineEnqueue(Customer customer)
     {
         orderLine.Enqueue(customer);
+        Debug.Log("Customer in line to place an order. Total customers waiting to place an order: " + orderLine.Count);
         return orderLine.Count;
     }
 
     public int BathroomLineEnqueue(Customer customer)
     {
         bathroomLine.Enqueue(customer);
+        Debug.Log("Customer in line for the bathroom. Total customers  waiting for bathroom: " + bathroomLine.Count);
         return bathroomLine.Count;
     }
 
+    public int BathroomLineDequeue() {
+        Customer customer = bathroomLine.Dequeue();
+
+        Debug.Log("Customer in bathroom. Total customers waiting for bathroom: " + bathroomLine.Count);
+        return bathroomLine.Count;
+    }
+
+    IEnumerator UseBathroom(Customer customer) {
+        this.bathroomVacant = false;
+        this.customerInBathroom = customer;
+        float timeInBathroom = Random.Range(this.bathroomWaitMinInterval, this.bathroomWaitMaxInterval);
+        yield return new WaitForSeconds(timeInBathroom);
+        this.bathroomVacant = true;
+        this.customerInBathroom = null;
+        customer.UseBathroom();
+    }
 }
